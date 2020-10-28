3279b7937646e3cd2f6a0d567cb20c1bf3770509
Leon Espinosa <leonespcast@gmail.com>
2020-10-28 14:32:45 +0100

    all function in MiSiCapp_main v 0.2.8

--- MiSiCapp/MiSiCapp_main.py
+++ MiSiCapp/MiSiCapp_main.py
@@ -29,6 +29,71 @@ gdict = {"gDir":"", "gfilename" : os.path.join("~", "out.tif"), "gdims" : None, 
 
 misic = MiSiC()
 
+def updatemeta(metadict = gdict, idx = 1):
+    viewer.layers[idx].metadata = metadict
+
+
+def seg_img(im, scale=1, noise="0.000", invert=True, frame=0, save=False, threshold=220):
+    
+    noise = float(noise)
+    rtim = np.zeros(gdict["gdims"])
+    p = gdict["gpos"]
+    global thresh
+    thresh = threshold
+
+    if save:
+        print("running...")
+        hyperS = np.zeros(im.shape)
+        for iT in range(im.shape[0]) :
+            imp = im[iT,:,:]
+            sr,sc = imp.shape
+            imp = rescale(imp,scale)
+            if noise > 0 : imp = random_noise(imp,mode = 'gaussian',var = noise)
+            imp = normalize2max(imp)
+            y1 = misic.segment(imp,invert = invert)
+            y = np.zeros((sr,sc,2))
+            y[:,:,0] = resize(rescale(y1[:,:,0],1.0/scale),(sr,sc))
+            y[:,:,1] = resize(rescale(y1[:,:,1],1.0/scale),(sr,sc))
+            if rtim.ndim == 3 : rtim[iT,:,:] = (y[:,:,0])
+            elif rtim.ndim == 4 : rtim[iT,p[1],:,:] = (y[:,:,0])
+            else: rtim[iT,p[1],p[2],:,:] = (y[:,:,0])
+            hyperS[iT,:,:] =  (y[:,:,0])
+            print("time = ", iT)
+            #print("Finish")
+            savestr = "all"
+    
+    
+
+    else :
+        print("running...")
+        hyperS = np.zeros(im.shape)
+        sr,sc = im.shape
+        im = rescale(im,scale)
+        if noise > 0 : im = random_noise(im, mode = 'gaussian', var = noise)
+        im = normalize2max(im)
+        y1 = misic.segment(im,invert = invert)
+        y = np.zeros((sr,sc,2))
+        y[:,:,0] = resize(rescale(y1[:,:,0],1.0/scale),(sr,sc))
+        y[:,:,1] = resize(rescale(y1[:,:,1],1.0/scale),(sr,sc))
+        if rtim.ndim == 2 : rtim[:,:] = (y[:,:,0])
+        elif rtim.ndim == 3 : rtim[p[0],:,:] = (y[:,:,0])
+        elif rtim.ndim == 4 : rtim[p[0],p[1],:,:] = (y[:,:,0])
+        else: rtim[p[0],p[1],p[2],:,:] = (y[:,:,0])
+        hyperS =  (y[:,:,0])
+        savestr = str(frame)
+    
+
+    gshape = viewer.layers[-1].metadata["gdims"]
+    gshape = "_".join([str(i) for i in gshape])
+
+    width = str(gdict["width"])
+
+    imsave(str(gdict["gfilename"])+"_W="+width+"_N="+str(noise)+"_DIM="+gshape+"_frame="+savestr+"_mask.tif",(255.0*hyperS).astype(np.uint8))
+    print("Finish")
+    return((255.0*rtim).astype(np.uint8))
+
+
+
 def main():
     with napari.gui_qt():
         viewer = napari.Viewer()
@@ -79,8 +144,8 @@ def main():
                 gdict["gpos"] = p
                 gdict["gsave_all"] = process_all
 
-                #updatemeta(gdict, 0)
-                return seg_img(img, misic, scale=round(10/mean_width, 2), noise=noise, invert=PhaseContrast, frame = viewer.dims.point[0], save=process_all)
+                updatemeta(gdict, 0)
+                return seg_img(img, scale=round(10/mean_width, 2), noise=noise, invert=PhaseContrast, frame = viewer.dims.point[0], save=process_all)
 
             else:
                 if layer.data.ndim == 5 : img = layer.data[p[0], p[1], p[2],:,:]
@@ -95,8 +160,8 @@ def main():
                 gdict["gpos"] = p
                 gdict["gsave_all"] = process_all
 
-                #updatemeta(gdict, 0)
-                return seg_img(img, misic, scale=round(10/mean_width, 2), noise=noise, invert=PhaseContrast, frame = viewer.dims.point[0], save=process_all)
+                updatemeta(gdict, 0)
+                return seg_img(img, scale=round(10/mean_width, 2), noise=noise, invert=PhaseContrast, frame = viewer.dims.point[0], save=process_all)
         
 
         viewer.grid_view()
