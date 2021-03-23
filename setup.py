862307d7f842ac09a4c9b6d80c87d253eb534910
Leon Espinosa <leonespcast@gmail.com>
2021-03-23 16:51:35 +0100

    setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="MiSiCgui", # Replace with your own username
-    version="0.3.7",
+    version="0.1.2-alpha",
     author="L.Espinosa",
     author_email="leonespcast@gmail.com",
     description="Microbe segmentation in dense colonies graphical interface using Napari",
