205a696b4971de9ff51e228129117ea79819c091
Leon Espinosa <leonespcast@gmail.com>
2021-04-15 20:08:01 +0200

    modif by model

--- MiSiCgui/MiSiCgui_main.py
+++ MiSiCgui/MiSiCgui_main.py
@@ -1,7 +1,15 @@
+
 import argparse 
 import warnings
 
+
 import os, sys
+from os.path import dirname, basename, isfile, join
+import importlib
+import importlib.util
+from pathlib import Path
+
+import glob
 from pathlib import Path
 
 from skimage.io import imsave,imread
@@ -23,13 +31,30 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 from MiSiC.MiSiC import *
+import models
 #import MiSiCgui
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 gdict = {"gDir":"", "gfilename" : os.path.join("~", "out.tif"), "gdims" : None, "width" : None, "gnoise" : None, "gthresh":220, "ginvert" : None, "gpos" : None, "gsave_all" : None}
 
-misic = MiSiC()
+
+p = Path(__file__).parents[1]
+p = join(p, "MiSiCgui", "models")
+print(p)
+modpaths = glob.glob(join(Path(p), "*.py"))
+MODELS = [ basename(f)[:-3] for f in modpaths if isfile(f) and not f.endswith('__init__.py')]
+print(MODELS)
+
+mmodd = MODELS[0]
+#modpath = modpaths[MODELS.index("misic_synthetic_b")]
+amodel = "models."+ mmodd
+currentModel = importlib.import_module(amodel)
+
+misic = currentModel.SegModel()
+
+#misic = MiSiC()
+#amodel = "/Users/leon/ownCloud/MacrosDropBox/pytoapp/MiSiCmodels/models/MiSiDC04082020.py"
 viewer = None
 
 def updatemeta(metadict = gdict, idx = 1):
---
+++ MiSiCgui/models/MiSiDC04082020.py
@@ -0,0 +1,53 @@
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import get_file
+
+import numpy as np
+
+from skimage.transform import resize,rescale
+from skimage.feature import shape_index
+from skimage.util import pad,random_noise
+from skimage.io import imread,imsave
+
+import matplotlib.pyplot as plt
+
+from MiSiC.utils import *
+#from utils_gui import *
+
+class SegModel():
+    def __init__(self):
+        self.size = 256
+        self.scalesvals = [1,1.5,2.0]
+        #self.model = load_model(model_name)
+        model_path = get_file(
+            'MiSiDC04082020',
+            'https://github.com/pswapnesh/Models/raw/master/MiSiDC04082020.h5', cache_dir='./cache')
+        self.model = load_model(model_path,compile=False)
+    
+    def preprocess(self,im):
+        n = len(self.scalesvals)
+        sh = np.zeros((im.shape[0],im.shape[1],n))
+        
+        if np.max(im) ==0:
+            return sh
+        pw = 15
+        im = pad(im,pw,'reflect')
+        sh = np.zeros((im.shape[0],im.shape[1],n))    
+        for i in range(n):
+            sh[:,:,i] = shape_index(im,self.scalesvals[i])
+     
+        return sh[pw:-pw,pw:-pw,:]
+    
+    def segment(self,im,invert = False):
+        im = normalize2max(im)        
+        pw = 16
+        if invert:
+            im = 1.0-im
+        im = pad(im,pw,'reflect')
+        sh = self.preprocess(im)
+
+        tiles,params = extract_tiles(sh,size = self.size,padding = 8)
+
+        yp = self.model.predict(tiles)
+
+        return stitch_tiles(yp,params)[pw:-pw,pw:-pw,:]
