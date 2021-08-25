6a530cc3ea99797ba78f13d2fd0f3b64226900d8
Leon Espinosa <leonespcast@gmail.com>
2021-08-25 10:33:40 +0200

    mise a jour main

--- MiSiCgui/MiSiCgui_main.py
+++ MiSiCgui/MiSiCgui_main.py
@@ -36,7 +36,7 @@ from MiSiCgui.utils import *
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-gdict = {"gDir":"", "gfilename" : os.path.join("~", "out.tif"), "gdims" : None, "width" : None, "gnoise" : None, "gthresh":220, "ginvert" : None, "gpos" : None, "gsave_all" : None}
+gdict = {"gDir":"~", "gfilename" : os.path.join("~", "out.tif"), "gdims" : None, "width" : None, "gnoise" : None, "gthresh":220, "ginvert" : None, "gpos" : None, "gsave_all" : None}
 
 
 p = Path(__file__).parents[1]
@@ -118,174 +118,173 @@ def seg_img(im, scale=1, noise="0.000", invert=True, frame=0, save=False, thresh
     return((255.0*hyperS).astype(np.uint8))
 
 def main():
-    with napari.gui_qt():
-        global viewer
+    #with napari.gui_qt():
+    global viewer
+    global gdict
+    viewer = napari.Viewer()
+
+    def updatelayer(layer):
+        viewer.layers[-1].metadata=gdict
+        updatelayer.reset_choices("layer")
+        viewer.layers[-1].metadata=gdict
+        print("event", layer)
+
+    def defaultpath(event_apath):
+        apath = filepicker.filename.value
+        gdict["gDir"] = apath
+        if len(viewer.layers) > 0 :
+            gdict["gfilename"] = os.path.join(apath, viewer.layers.selection.active.name)
+    
+    def changelabels(event_thr):
+
+        print(make_labels.threshold.value)
+        thresh = make_labels.threshold.value
+        laynames = [ l.name for l in viewer.layers]
+        #selects = [l.selected for l in viewer.layers]
+        idx = laynames.index(viewer.layers.selection.active.name)
+        #idx = selects.index(True)
+        
+        #if ('image_mask result' in laynames)  :
+            #im = viewer.layers['image_mask result'].data > (thresh)
+        im = viewer.layers[viewer.layers.selection.active.name].data > (thresh)
+        label_image = label(im)
+            #viewer.add_labels(label_image, name="seg")
+        gdict["gthresh"] = thresh
+        
+        if ('seg' in laynames):
+            viewer.layers['seg'].data = label_image
+            #i = laynames.index("seg")
+            #viewer.layers.pop(i)
+            if viewer.layers['seg'] in viewer.layers.selection :
+                viewer.layers.selection.remove(viewer.layers['seg'])
+            viewer.layers.selection.add(viewer.layers[idx])
+        else :
+            viewer.add_labels(label_image, name="seg")
+            if viewer.layers['seg'] in viewer.layers.selection :
+                viewer.layers.selection.remove(viewer.layers['seg'])
+            viewer.layers.selection.add(viewer.layers[idx])
+
+        #viewer.layers['seg'].selected = False
+        #viewer.layers.selection.remove(viewer.layers['seg'])
+        #viewer.layers[idx].selected = True
+        #viewer.layers.selection.add(viewer.layers[idx])
+
+
+
+    @magicgui(call_button="get_mask", layout="vertical")
+    def image_mask(layer: Image, mean_width = 6, noise = "0.00", PhaseContrast = True, process_all = False) -> 'napari.types.ImageData':
         global gdict
-        viewer = napari.Viewer()
+        laynames = [ l.name for l in viewer.layers]
+        idx = laynames.index(viewer.layers.selection.active.name)
 
-        def updatelayer(layer):
-            viewer.layers[-1].metadata=gdict
-            updatelayer.reset_choices("layer")
-            viewer.layers[-1].metadata=gdict
-            print("event", layer)
+        p = tuple([int(round(x)) for x in viewer.dims.point])
+        #print("pressed", viewer.layers)
+        if process_all & (layer.data.ndim > 2) :
+            if layer.data.ndim == 5 : img = layer.data[:, p[1], p[2],:,:]
+            elif layer.data.ndim == 4 : img = layer.data[:, p[1],:,:]
+            elif layer.data.ndim == 3 : img = layer.data[:,:,:]
+            else: img = layer.data
+            gdict["gfilename"] = os.path.join(gdict["gDir"], layer.name)
+            gdict["gdims"] = layer.data.shape
+            gdict["width"] = mean_width
+            gdict["gnoise"] = noise
+            gdict["ginvert"] = PhaseContrast
+            gdict["gpos"] = p
+            gdict["gsave_all"] = process_all
 
-        def defaultpath(event_apath):
-            apath = filepicker.filename.value
-            gdict["gDir"] = apath
-            if len(viewer.layers) > 0 :
-                gdict["gfilename"] = os.path.join(apath, viewer.layers.selection.active.name)
+            updatemeta(gdict, idx)
+            return seg_img(img, scale=round(10/mean_width, 2), noise=noise, invert=PhaseContrast, frame = viewer.dims.point[0], save=process_all)
+
+        else:
+            if layer.data.ndim == 5 : img = layer.data[p[0], p[1], p[2],:,:]
+            elif layer.data.ndim == 4 : img = layer.data[p[0], p[1],:,:]
+            elif layer.data.ndim == 3 : img = layer.data[p[0],:,:]
+            else: img = layer.data
+            gdict["gfilename"] = os.path.join(gdict["gDir"], layer.name)
+            gdict["gdims"] = layer.data.shape
+            gdict["width"] = mean_width
+            gdict["gnoise"] = noise
+            gdict["ginvert"] = PhaseContrast
+            gdict["gpos"] = p
+            gdict["gsave_all"] = process_all
+
+            updatemeta(gdict, idx)
+
+            return seg_img(img, scale=round(10/mean_width, 2), noise=noise, invert=PhaseContrast, frame = viewer.dims.point[0], save=process_all)
+    
+    viewer.window.add_dock_widget(image_mask, area="right")
+    viewer.layers.events.changed.connect(updatelayer)
+
+
+    @magicgui(
+        auto_call=True,
+        threshold={"widget_type": "IntSlider", 'min': 1, 'max': 255, 'orientation':"horizontal"},
+    )
+    def make_labels(threshold = 220):
+        return threshold
+
+    viewer.window.add_dock_widget(make_labels, area="bottom")
+    make_labels.threshold.changed.connect(changelabels)
+    
+
+
+    @magicgui(call_button="save_labels")
+    def funcsave_labels():
+        imsave(str(gdict["gfilename"])+"_thresh_"+str(gdict["gthresh"])+"_labels.tif",(-4294967295.0*viewer.layers["seg"].data).astype(np.uint32))
+        return None
+
+    viewer.window.add_dock_widget(funcsave_labels, area="bottom")
+
+    @magicgui(call_button="HELP")
+    def funcHELP():
+        #print("debut")
+        msg = QMessageBox()
+        msg.setWindowTitle("Help1")
+        msg.setText("read Napari : https://napari.org/tutorials/\n"+
+        "read the doc : https://github.com/leec13/MiSiCgui\n"+
+        "0 - drag and drop the image\n"
+        "1 - select default directory\n"+
+        "2 - Add a shape layer\n"+
+        "3 - trace some widths\n"+
+        "4 - get the mean width (button)\n"+
+        "5 - set the mean width (field)\n"+
+        "6 - get the mask\n"+
+        "7 - to create segmentation mouve threshold cursor (only 2D and Time Lapse)")
+        msg.exec_()
         
-        def changelabels(event_thr):
+        return None
+    
+    viewer.window.add_dock_widget(funcHELP, area="bottom")
 
-            print(make_labels.threshold.value)
-            thresh = make_labels.threshold.value
-            laynames = [ l.name for l in viewer.layers]
-            #selects = [l.selected for l in viewer.layers]
-            idx = laynames.index(viewer.layers.selection.active.name)
-            #idx = selects.index(True)
-            
-            #if ('image_mask result' in laynames)  :
-                #im = viewer.layers['image_mask result'].data > (thresh)
-            im = viewer.layers[viewer.layers.selection.active.name].data > (thresh)
-            label_image = label(im)
-                #viewer.add_labels(label_image, name="seg")
-            gdict["gthresh"] = thresh
-            
-            if ('seg' in laynames):
-                viewer.layers['seg'].data = label_image
-                #i = laynames.index("seg")
-                #viewer.layers.pop(i)
-                if viewer.layers['seg'] in viewer.layers.selection :
-                    viewer.layers.selection.remove(viewer.layers['seg'])
-                viewer.layers.selection.add(viewer.layers[idx])
-            else :
-                viewer.add_labels(label_image, name="seg")
-                if viewer.layers['seg'] in viewer.layers.selection :
-                    viewer.layers.selection.remove(viewer.layers['seg'])
-                viewer.layers.selection.add(viewer.layers[idx])
-
-            #viewer.layers['seg'].selected = False
-            #viewer.layers.selection.remove(viewer.layers['seg'])
-            #viewer.layers[idx].selected = True
-            #viewer.layers.selection.add(viewer.layers[idx])
-
-
-
-        @magicgui(call_button="get_mask", layout="vertical")
-        def image_mask(layer: Image, mean_width = 6, noise = "0.00", PhaseContrast = True, process_all = False) -> 'napari.types.ImageData':
-            global gdict
-            laynames = [ l.name for l in viewer.layers]
-            idx = laynames.index(viewer.layers.selection.active.name)
-
-            p = tuple([int(round(x)) for x in viewer.dims.point])
-            #print("pressed", viewer.layers)
-            if process_all & (layer.data.ndim > 2) :
-                if layer.data.ndim == 5 : img = layer.data[:, p[1], p[2],:,:]
-                elif layer.data.ndim == 4 : img = layer.data[:, p[1],:,:]
-                elif layer.data.ndim == 3 : img = layer.data[:,:,:]
-                else: img = layer.data
-                gdict["gfilename"] = os.path.join(gdict["gDir"], layer.name)
-                gdict["gdims"] = layer.data.shape
-                gdict["width"] = mean_width
-                gdict["gnoise"] = noise
-                gdict["ginvert"] = PhaseContrast
-                gdict["gpos"] = p
-                gdict["gsave_all"] = process_all
-
-                updatemeta(gdict, idx)
-                return seg_img(img, scale=round(10/mean_width, 2), noise=noise, invert=PhaseContrast, frame = viewer.dims.point[0], save=process_all)
-
-            else:
-                if layer.data.ndim == 5 : img = layer.data[p[0], p[1], p[2],:,:]
-                elif layer.data.ndim == 4 : img = layer.data[p[0], p[1],:,:]
-                elif layer.data.ndim == 3 : img = layer.data[p[0],:,:]
-                else: img = layer.data
-                gdict["gfilename"] = os.path.join(gdict["gDir"], layer.name)
-                gdict["gdims"] = layer.data.shape
-                gdict["width"] = mean_width
-                gdict["gnoise"] = noise
-                gdict["ginvert"] = PhaseContrast
-                gdict["gpos"] = p
-                gdict["gsave_all"] = process_all
-
-                updatemeta(gdict, idx)
-
-                return seg_img(img, scale=round(10/mean_width, 2), noise=noise, invert=PhaseContrast, frame = viewer.dims.point[0], save=process_all)
+    @magicgui(mean_width = {'bind': ""}, call_button="get_WIDTH", result_widget=True, labels = False)
+    def meanfunc(mean_width):
         
-        viewer.window.add_dock_widget(image_mask, area="right")
-        viewer.layers.events.changed.connect(updatelayer)
-
-
-        @magicgui(
-            auto_call=True,
-            threshold={"widget_type": "IntSlider", 'min': 1, 'max': 255, 'orientation':"horizontal"},
-            #value = {"fixedWidth": 50}
-        )
-        def make_labels(threshold = 220, value = "220"):
-            return threshold
-
-        viewer.window.add_dock_widget(make_labels, area="bottom")
-        make_labels.threshold.changed.connect(changelabels)
+        if (viewer.layers[-1].name == "Shapes") & (viewer.layers[-1] in viewer.layers.selection) :
+            data = viewer.layers[-1].data
+            c = 0
+            for d in data:
+                c += (sum((d[1,:] - d[0,:])**2))**0.5
+                res_mean = (str(round(c/len(data))))
         
-
-
-        @magicgui(call_button="save_labels")
-        def funcsave_labels():
-            imsave(str(gdict["gfilename"])+"_thresh_"+str(gdict["gthresh"])+"_labels.tif",(-4294967295.0*viewer.layers["seg"].data).astype(np.uint32))
-            return None
-
-        viewer.window.add_dock_widget(funcsave_labels, area="bottom")
-
-        @magicgui(call_button="HELP")
-        def funcHELP():
-            #print("debut")
-            msg = QMessageBox()
-            msg.setWindowTitle("Help1")
-            msg.setText("read Napari : https://napari.org/tutorials/\n"+
-            "read the doc : https://github.com/leec13/MiSiCgui\n"+
-            "0 - drag and drop the image\n"
-            "1 - select default directory\n"+
-            "2 - Add a shape layer\n"+
-            "3 - trace some widths\n"+
-            "4 - get the mean width (button)\n"+
-            "5 - set the mean width (field)\n"+
-            "6 - get the mask\n"+
-            "7 - to create segmentation mouve threshold cursor (only 2D and Time Lapse)")
-            msg.exec_()
-           
-            return None
+        else :
+            res_mean = ""
         
-        viewer.window.add_dock_widget(funcHELP, area="bottom")
+        return res_mean
 
-        @magicgui(mean_width = {'bind': ""}, call_button="get_WIDTH", result_widget=True, labels = False)
-        def meanfunc(mean_width):
-            
-            if (viewer.layers[-1].name == "Shapes") & (viewer.layers[-1] in viewer.layers.selection) :
-                data = viewer.layers[-1].data
-                c = 0
-                for d in data:
-                    c += (sum((d[1,:] - d[0,:])**2))**0.5
-                    res_mean = (str(round(c/len(data))))
-            
-            else :
-                res_mean = ""
-            
-            return res_mean
-
-        
-        # alternative to mean_width = {'bind': ""} ligne suivante
-        # meanfunc.mean_width.bind("")
-        viewer.window.add_dock_widget(meanfunc, area="right")
+    
+    # alternative to mean_width = {'bind': ""} ligne suivante
+    # meanfunc.mean_width.bind("")
+    viewer.window.add_dock_widget(meanfunc, area="right")
 
 
-        @magicgui(auto_call=True, filename={"mode": "d"})
-        def filepicker(filename=Path("~")):
-            """doc string test"""
-            return filename
-       
-        viewer.window.add_dock_widget(filepicker, area="right")
-        filepicker.filename.changed.connect(defaultpath)
-
+    @magicgui(auto_call=True, filename={"mode": "d"})
+    def filepicker(filename=Path("~")):
+        """doc string test"""
+        return filename
+    
+    viewer.window.add_dock_widget(filepicker, area="right")
+    filepicker.filename.changed.connect(defaultpath)
+    napari.run()
 
 
 if __name__ == "__main__":
